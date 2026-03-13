f5e5f915867b55df402f94b4e016b9c21ac536ad
Ramya <ramya@narveetech.com>
2026-03-13 09:29:15 +0530

    updated

--- src/main/java/com/example/DTO/NetTerm.java
+++ src/main/java/com/example/DTO/NetTerm.java
@@ -2,7 +2,13 @@ package com.example.DTO;
 
 public enum NetTerm {
 
-	NET_7(7, "1 Week"), NET_14(14, "2 Weeks"), NET_30(30, "1 Month"), NET_45(45, "45 Days");
+	NET_7(7, "1 Week"),
+	NET_14(14, "2 Weeks"),
+	NET_30(30, "1 Month"),
+	NET_45(45, "45 Days"),
+	NET_60(60, "60 Days"),
+	NET_75(75, "75 Days"),
+	NET_120(120, "120 Days");
 
 	private final int days;
 	private final String label;
